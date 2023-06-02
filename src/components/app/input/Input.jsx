9011ce979e7701617c232f6e7d09d7d5221968b1
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-06-01 22:10:25 -0400

    feat: "WorkingLogin"

--- src/components/app/input/Input.jsx
+++ src/components/app/input/Input.jsx
@@ -1,3 +1,19 @@
-import { Input } from 'antd';
-const App = () => <Input placeholder="Basic usage" />;
-export default App;
+import React from "react";
+
+const Input = ({labelValue, Id, type = 'text', placeholder, classNameInput = "form-control", classNameLabel = 'form-label'}) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={`${Id && Id}`} className={`${classNameLabel}`}>
+        {labelValue && labelValue}
+      </label>
+      <input
+        type={`${type}`}
+        className={`${classNameInput}`}
+        id={`${Id && Id}`}
+        placeholder={`${placeholder && placeholder}`}
+      />
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import Input from "../../components/app/input/Input";
+import './authStyles.css'
 
 const Login = () => {
+  const InputsArray = [
+    {
+      Id: "email",
+      type: "email",
+      placeholder: "Enter your Email",
+      classNameInput: "form-control",
+      classNameLabel: "form-label",
+      labelValue: "Email",
+    },
+    {
+      Id: "password",
+      type: "password",
+      placeholder: "Enter your Password",
+      classNameInput: "form-control",
+      classNameLabel: "form-label",
+      labelValue: "Password",
+    },
+  ];
+
   return (
-    <section className="py-5" style={{ background: "#ffd333" }}>
-      <article className="my-5 w-25 rounded-3 mx-auto p-3">
-        
+    <section className="py-5 form-login-background" style={{ minHeight: "100vh", padding: '1em 1em 1em 1em'}}>
+      <article className="rounded-3 mx-auto p-4 form-login-container">
+        <h3 className="text-center">Login</h3>
+        <p className="text-center">Login to your account to continue.</p>
+        <form action="">
+          {InputsArray.map((item, index) => (
+            <div key={index}>
+              <Input
+                Id={item.Id}
+                type={item.type}
+                placeholder={item.placeholder}
+                classNameInput={item.classNameInput}
+                classNameLabel={item.classNameLabel}
+                labelValue={item.labelValue}
+              />
+            </div>
+          ))}
+          <button 
+          type="submit" 
+          className="border-0 px-3 py-2 btn"
+          style={{background: "var(--color-red-wine)", color: "var(--color-white)"}}
+          >    
+          Login    
+          </button>
+        </form>
       </article>
     </section>
   );
