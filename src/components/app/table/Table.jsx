38f55cf8b653848986fc7f67e7e21db56d727d44
Evertz Hamilton <hamiltonevertz99@gmail.com>
2023-06-13 16:51:55 -0400

    feat: working on

--- src/components/app/table/Table.jsx
+++ src/components/app/table/Table.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import { Table } from 'antd';
+import { useState } from 'react';
 
-const Table = () => {
+/**
+ * 
+ * @typedef {Object} ITable
+ * @property {Array} data
+ * @property {Array} columns
+ * 
+ * @param {ITable} props
+ */
+
+
+const TableComponent = ({ data=[], columns=[] }) => {
+
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: 'odd',
+        text: 'Select Odd Row',
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: 'even',
+        text: 'Select Even Row',
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+
   return (
-    <div>Table</div>
+    <Table 
+    rowSelection={rowSelection} 
+    columns={columns} 
+    dataSource={data} 
+    />
   )
 }
 
-export default Table
+export default TableComponent
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,110 +2,140 @@ import React from 'react'
 import './style.css'
 import CardHeader from '../../components/pages/dashboard/dashboardCardHeader/CardHeader'
 import GraphicColumns from '../../components/pages/dashboard/dashboardGraphic/GraphicColumns'
-import { Table } from 'antd';
+import TableComponent from '../../components/app/table/Table'
+
+//! Card of Header
+const CardHeaderInfo = [
+  {
+    title: 'Total',
+    amount: 1000,
+    comparedText: 'compared to Abril 2021',
+    porcent: 47.7
+  },
+  {
+    title: 'Total',
+    amount: 2000,
+    comparedText: 'compared to Abril 2021',
+    porcent: 47.7
+  },
+  {
+    title: 'Total',
+    amount: 3000,
+    comparedText: 'compared to Abril 2021',
+    porcent: 47.7
+  },
+]
+
+//! Data for Graphic 
+
+const dataGraphic = [
+  {
+    type: 'Jan',
+    sales: 38,
+  },
+  {
+    type: 'Feb',
+    sales: 52,
+  },
+  {
+    type: 'Mar',
+    sales: 61,
+  },
+  {
+    type: 'Apr',
+    sales: 145,
+  },
+  {
+    type: 'May',
+    sales: 48,
+  },
+  {
+    type: 'Jun',
+    sales: 38,
+  },
+  {
+    type: 'Jul',
+    sales: 38,
+  },
+  {
+    type: 'Aug',
+    sales: 38,
+  },
+  {
+    type: 'Sep',
+    sales: 38,
+  },
+  {
+    type: 'Oct',
+    sales: 38,
+  },
+  {
+    type: 'Nov',
+    sales: 38,
+  },
+  {
+    type: 'Dec',
+    sales: 38,
+  },
+];
+
+const config = {
+  data: dataGraphic,
+  xField: 'type',
+  yField: 'sales',
+  label: {
+    // 可手动配置 label 数据标签位置
+    position: 'middle',
+    // 'top', 'bottom', 'middle',
+    // 配置样式
+    style: {
+      fill: '#FFFFFF',
+      opacity: 0.6,
+    },
+  },
+  xAxis: {
+    label: {
+      autoHide: true,
+      autoRotate: false,
+    },
+  },
+  meta: {
+    type: {
+      alias: '类别',
+    },
+    sales: {
+      alias: '销售额',
+    },
+  },
+};
+
+//! Data for Table 
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+const dataTable = [];
+for (let i = 0; i < 46; i++) {
+  dataTable.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 
 const Dashboard = () => {
-
-  const CardHeaderInfo = [
-    {
-      title: 'Total',
-      amount: 1000,
-      comparedText: 'compared to Abril 2021',
-      porcent: 47.7
-    },
-    {
-      title: 'Total',
-      amount: 2000,
-      comparedText: 'compared to Abril 2021',
-      porcent: 47.7
-    },
-    {
-      title: 'Total',
-      amount: 3000,
-      comparedText: 'compared to Abril 2021',
-      porcent: 47.7
-    },
-  ]
-  const data = [
-    {
-      type: 'Jan',
-      sales: 38,
-    },
-    {
-      type: 'Feb',
-      sales: 52,
-    },
-    {
-      type: 'Mar',
-      sales: 61,
-    },
-    {
-      type: 'Apr',
-      sales: 145,
-    },
-    {
-      type: 'May',
-      sales: 48,
-    },
-    {
-      type: 'Jun',
-      sales: 38,
-    },
-    {
-      type: 'Jul',
-      sales: 38,
-    },
-    {
-      type: 'Aug',
-      sales: 38,
-    },
-    {
-      type: 'Sep',
-      sales: 38,
-    },
-    {
-      type: 'Oct',
-      sales: 38,
-    },
-    {
-      type: 'Nov',
-      sales: 38,
-    },
-    {
-      type: 'Dec',
-      sales: 38,
-    },
-  ];
-
-  const config = {
-    data,
-    xField: 'type',
-    yField: 'sales',
-    label: {
-      // 可手动配置 label 数据标签位置
-      position: 'middle',
-      // 'top', 'bottom', 'middle',
-      // 配置样式
-      style: {
-        fill: '#FFFFFF',
-        opacity: 0.6,
-      },
-    },
-    xAxis: {
-      label: {
-        autoHide: true,
-        autoRotate: false,
-      },
-    },
-    meta: {
-      type: {
-        alias: '类别',
-      },
-      sales: {
-        alias: '销售额',
-      },
-    },
-  };
 
   return (
     <section className="dashboard">
@@ -117,6 +147,7 @@ const Dashboard = () => {
           {
             CardHeaderInfo.map((item, index) => (
               <CardHeader
+                key={index}
                 title={`${item.title}`}
                 amount={item.amount}
                 comparedText={`${item.comparedText}`}
@@ -134,6 +165,9 @@ const Dashboard = () => {
       </section>
       <section className='mt-4'>
           <h3 className='mb-4'>Recent Orders</h3>
+          <article>
+            <TableComponent data={dataTable} columns={columns}/>
+          </article>
       </section>
     </section>
   )
