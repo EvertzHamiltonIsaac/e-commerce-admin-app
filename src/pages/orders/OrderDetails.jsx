c6e5248772416c980d9a4515c2bc0a262c03c0a1
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-08-03 00:37:14 -0400

    feat: Admin Finished Without user logged

--- src/pages/orders/OrderDetails.jsx
+++ src/pages/orders/OrderDetails.jsx
@@ -13,6 +13,8 @@ import Select from "../../components/app/select/Select";
 import "./orderStyles.css";
 import { FormatDateTime } from "../../utils/DateTimeFormater";
 import { toast } from "react-toastify";
+import { useTokenExpired } from "../../hooks/useTokenExpired";
+
 const OptionSelectStatus = [
   {
     title: "Ordered",
@@ -42,9 +44,8 @@ const OrderDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
 
-  const { order, orderUpdated, isError, isSuccess, isLoading } = useSelector(
-    (state) => state.orders
-  );
+  const { order, orderUpdated, isError, isSuccess, isLoading, message } =
+    useSelector((state) => state.orders);
 
   const handleChangeStatus = (e) => {
     console.log(e.target.value);
@@ -58,6 +59,11 @@ const OrderDetails = () => {
     );
   };
 
+  const handleReset = () => {
+    setInitials("");
+    history.go(-1);
+  };
+
   useEffect(() => {
     if (orderUpdated && isSuccess) {
       toast.success("Order Status Updated Succesfully!");
@@ -69,6 +75,8 @@ const OrderDetails = () => {
       toast.error("Something Went Wrong!");
     }
   }, [isSuccess, isError, isLoading]);
+
+  const isTokenExpired = useTokenExpired(message, isError);
 
   useEffect(() => {
     dispatch(getOrderById(id));
@@ -100,10 +108,7 @@ const OrderDetails = () => {
               <h1>Orders</h1>
               <h6 className="text-muted">{`On this page are all the orders made in the project. In General there are amount of orders.`}</h6>
             </div>
-            <div
-              className="btn_back shadow rounded"
-              onClick={() => history.go(-1)}
-            >
+            <div className="btn_back shadow rounded" onClick={handleReset}>
               <FontAwesomeIcon icon={faArrowLeft} />
               <span>Back</span>
             </div>
@@ -219,10 +224,134 @@ const OrderDetails = () => {
                 </div>
               </div>
             </div>
-            <div className="shipping_info shadow pt-4 user-select-none rounded">
-              <div className="shipping_info_header p-3">
-                <h4>Shipping Information</h4>
-                <h6 className="text-muted">{`On this page are all the orders made in the project. In General there are amount of orders.`}</h6>
+
+            <div className="payment_info_container rounded shadow ">
+              <div style={{ padding: "1em 1em 1em 1em" }}>
+                <h5>Payment Information</h5>
+                <h6 className="text-muted">{`General Information about Payment of order`}</h6>
+              </div>
+              <div
+                style={{
+                  backgroundColor: "lightgray",
+                  height: "1px",
+                  width: "100%",
+                }}
+              ></div>
+              <div className="payment_info">
+                <div className="payment_general_info">
+                  <span
+                    className="text-muted"
+                    style={{
+                      letterSpacing: "1.2px",
+                      fontSize: "12px",
+                      fontWeight: "650",
+                    }}
+                  >
+                    PAYMENT GENERAL INFO
+                  </span>
+                  <div className="payment_info_general_text mt-3">
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Amount:</span>
+                      <span>
+                        {order?.data?.paymentInfo?.amountMoney?.amount.toLocaleString(
+                            undefined,
+                            {
+                              style: "decimal",
+                              minimumFractionDigits: 2,
+                            }
+                          )}
+                      </span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Currency:</span>
+                      <span>
+                        {order?.data?.paymentInfo?.amountMoney?.currency}
+                      </span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted" style={{ width: "160px" }}>
+                        Payment Id:
+                      </span>
+                      <span>{order?.data?.paymentInfo?.paymentId}</span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Payment Status:</span>
+                      <span
+                        className={`text-light p-1 rounded ${
+                          order?.data?.paymentInfo?.paymentStatus ===
+                          "COMPLETED"
+                            ? "bg-success "
+                            : " bg-danger"
+                        }`}
+                      >
+                        {order?.data?.paymentInfo?.paymentStatus}
+                      </span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Receipt Number:</span>
+                      <span>{order?.data?.paymentInfo?.receiptNumber}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="card_detail">
+                  <span
+                    className="text-muted"
+                    style={{
+                      letterSpacing: "1.2px",
+                      fontSize: "12px",
+                      fontWeight: "650",
+                    }}
+                  >
+                    CARD DETAIL
+                  </span>
+                  <div className="card_detail_info_text pt-3">
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Card Brand:</span>
+                      <span>{order?.data?.paymentInfo?.cardDetails?.card?.cardBrand}</span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Card Type:</span>
+                      <span>{order?.data?.paymentInfo?.cardDetails?.card?.cardType}</span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Card Bin:</span>
+                      <span>
+                        {order?.data?.paymentInfo?.cardDetails?.card?.bin}
+                      </span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Card Status:</span>
+                      <span
+                        className={`text-light p-1 rounded ${
+                          order?.data?.paymentInfo?.cardDetails.cardDetailsStatus ===
+                          "CAPTURED"
+                            ? "bg-success "
+                            : " bg-danger"
+                        }`}
+                      >
+                        {order?.data?.paymentInfo?.cardDetails.cardDetailsStatus}
+                      </span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Authorized At:</span>
+                      <span>{FormatDateTime(order?.data?.cardPaymentTimeLine?.authorizedAt, "MMMM Do YYYY")}</span>
+                    </div>
+                    <div className="shipping_info_items">
+                      <span className="text-muted">Captured At:</span>
+                      <span>{FormatDateTime(order?.data?.cardPaymentTimeLine?.capturedAt, "MMMM Do YYYY")}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="shipping_info shadow user-select-none rounded mb-4">
+              <div
+                className="shipping_info_header"
+                style={{ padding: "1em 1em 0em 1em" }}
+              >
+                <h5>Shipping Information</h5>
+                <h6 className="text-muted">{`General Information about order`}</h6>
               </div>
               <div
                 style={{
@@ -242,7 +371,9 @@ const OrderDetails = () => {
                 </div>
                 <div className="shipping_info_items">
                   <span className="text-muted">Address:</span>
-                  <span>{order?.data?.shippingInfo?.address}</span>
+                  <span style={{ width: "160px" }}>
+                    {order?.data?.shippingInfo?.address}
+                  </span>
                 </div>
                 <div className="shipping_info_items">
                   <span className="text-muted">City:</span>
@@ -262,7 +393,8 @@ const OrderDetails = () => {
                 </div>
               </div>
             </div>
-            <div className="order_container shadow rounded">
+
+            <div className="order_container shadow rounded mb-4">
               <div className="order_info">
                 <div
                   style={{ display: "flex", gap: ".5em", alignItems: "center" }}
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -12,19 +12,28 @@ import {
   faHouseCircleCheck,
   faCartFlatbed,
   faEye,
-  faClipboardCheck
+  faClipboardCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import CardHeader from "../../components/pages/dashboard/dashboardCardHeader/CardHeader";
 import { Link } from "react-router-dom";
-import './orderStyles.css'
+import "./orderStyles.css";
 import { useTokenExpired } from "../../hooks/useTokenExpired";
 
 const Orders = () => {
   const dispatch = useDispatch();
 
   const [searchText, setSearchText] = useState("");
-  const { isLoading, AllOrders, isError, message } = useSelector((state) => state.orders);
+  const { isLoading, AllOrders, isError, message } = useSelector(
+    (state) => state.orders
+  );
   const OrdersData = [];
+
+  const OrderedOrders = [];
+  const ProcessedOrders = [];
+  const ShippedOrders = [];
+  const OutForDeliveryOrders = [];
+  const DeliveredOrders = [];
+
   for (let index = 0; index < AllOrders?.data?.length; index++) {
     OrdersData.push({
       key: index + 1,
@@ -41,10 +50,48 @@ const Orders = () => {
         style: "decimal",
         minimumFractionDigits: 0,
       }),
-      status: <div style={{color: 'white'}} className="rounded d-flex justify-content-center p-1 gap-1 align-items-center bg-dark">{AllOrders?.data[index]?.orderStatus}</div>,
+      status: (
+        <div
+          className={`
+          rounded 
+          d-flex 
+          justify-content-center 
+          p-1 
+          gap-1 
+          align-items-center 
+          ${
+            AllOrders?.data[index]?.orderStatus === 'Ordered'
+            ? 'bg-dark text-light'
+            : ''
+          }
+          ${
+            AllOrders?.data[index]?.orderStatus === 'Processed'
+            ? 'bg-secondary text-light'
+            : ''
+          }
+          ${
+            AllOrders?.data[index]?.orderStatus === 'Shipped'
+            ? 'bg-primary text-light'
+            : ''
+          }
+          ${
+            AllOrders?.data[index]?.orderStatus === 'Out for Delivery'
+            ? 'bg-warning text-dark'
+            : ''
+          }
+          ${
+            AllOrders?.data[index]?.orderStatus === 'Delivered'
+            ? 'bg-success text-light'
+            : ''
+          }
+          `}
+        >
+          {AllOrders?.data[index]?.orderStatus}
+        </div>
+      ),
       viewOrderDetails: (
         <Link
-        to={`./${AllOrders?.data[index]?._id}`}
+          to={`./${AllOrders?.data[index]?._id}`}
           className="orderView_btn rounded d-flex justify-content-center p-1 gap-1 align-items-center"
         >
           <FontAwesomeIcon icon={faEye} />
@@ -52,46 +99,64 @@ const Orders = () => {
         </Link>
       ),
     });
+
+    if (AllOrders?.data[index]?.orderStatus === "Ordered") {
+      OrderedOrders.push(AllOrders?.data[index]);
+    }
+    if (AllOrders?.data[index]?.orderStatus === "Processed") {
+      ProcessedOrders.push(AllOrders?.data[index]);
+    }
+    if (AllOrders?.data[index]?.orderStatus === "Shipped") {
+      ShippedOrders.push(AllOrders?.data[index]);
+    }
+    if (AllOrders?.data[index]?.orderStatus === "Out for Delivery") {
+      OutForDeliveryOrders.push(AllOrders?.data[index]);
+    }
+    if (AllOrders?.data[index]?.orderStatus === "Delivered") {
+      DeliveredOrders.push(AllOrders?.data[index]);
+    }
   }
 
   const CardHeaderInformation = [
     {
       title: "Ordered",
-      subTitle: `Total Orders Ordered: `,
+      subTitle: `Total Orders Ordered: ${OrderedOrders.length}`,
       icon: faCartShopping,
       classNameBackGroundColor: "bg-dark",
-      classNameColor: 'text-black'
+      classNameColor: "text-black",
     },
     {
       title: "Processed",
-      subTitle: `Total Orders Processed:  `,
+      subTitle: `Total Orders Processed: ${ProcessedOrders.length}`,
       icon: faClipboardCheck,
       classNameBackGroundColor: "bg-secondary",
-      classNameColor: 'text-secondary'
+      classNameColor: "text-secondary",
     },
     {
       title: "Shipped",
-      subTitle: `Total Orders Shipped: `,
+      subTitle: `Total Orders Shipped: ${ShippedOrders.length}`,
       icon: faCartFlatbed,
       classNameBackGroundColor: "bg-primary",
-      classNameColor: 'text-primary'
+      classNameColor: "text-primary",
     },
     {
       title: "Out for Delivery",
-      subTitle: `Orders Out for Delivery: `,
+      subTitle: `Orders Out for Delivery: ${OutForDeliveryOrders.length}`,
       icon: faTruckFast,
       classNameBackGroundColor: "bg-warning",
-      classNameColor: 'text-warning'
+      classNameColor: "text-warning",
     },
     {
       title: "Delivered",
-      subTitle: `Total Orders Delivered: `,
+      subTitle: `Total Orders Delivered: ${DeliveredOrders.length}`,
       icon: faHouseCircleCheck,
       classNameBackGroundColor: "bg-success",
-      classNameColor: 'text-success'
+      classNameColor: "text-success",
     },
   ];
+
   const isTokenExpired = useTokenExpired(message, isError);
+
   useEffect(() => {
     dispatch(getAllOrders());
   }, []);
